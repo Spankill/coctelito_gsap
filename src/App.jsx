2f1251d6a19541bca1a1d8fb76ca751ac627d541
ARUHIZA CARLOS <aruhiza87@gmail.com>
2026-05-20 16:41:56 -0400

    primera parte de la seccion hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
 
-import "./index.css";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
-import React from "react";
 
 const App = () => {
   return (
-    <div className="flex-center h-[100vh]">
-      <h1 className="text-3xl text-indigo-500">HOLITA GSsAP!</h1>
-    </div>
+    <main>
+      <Navbar />
+      <Hero /> 
+      <div className="h-dvh bg-black"></div>
+    </main>
   );
 };
 
