45fcbe60c34719f772a9bd7b6916afedc860db12
ARUHIZA CARLOS <aruhiza87@gmail.com>
2026-05-19 22:05:46 -0400

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+import "./index.css";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="flex-center h-[100vh]">
+      <h1 className="text-3xl text-indigo-500">HOLITA GSsAP!</h1>
+    </div>
+  );
+};
+
+export default App;
