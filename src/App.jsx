fad6f6c0b1eae7d66c4128b85eed8727de1cc270
ARUHIZA CARLOS <aruhiza87@gmail.com>
2026-05-20 22:43:08 -0400

    cocktail build

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
 
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Cocktails from "./components/Cocktails";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -12,6 +14,7 @@ const App = () => {
     <main>
       <Navbar />
       <Hero /> 
+      <Cocktails />
       <div className="h-dvh bg-black"></div>
     </main>
   );
