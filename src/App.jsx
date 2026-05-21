363f0d296e5edf673da63b0768ad4913aafaf7e7
ARUHIZA CARLOS <aruhiza87@gmail.com>
2026-05-21 18:15:31 -0400

    completando menuy contacto

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Cocktails from "./components/Cocktails";
 import About from "./components/About";
+import Art from "./components/Art";
+import Contact from "./components/Contact";
+import Menu from "./components/Menu";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -17,6 +20,9 @@ const App = () => {
       <Hero /> 
       <Cocktails />
       <About />
+      <Art />
+      <Menu />
+      <Contact />
       <div className="h-dvh bg-black"></div>
     </main>
   );
